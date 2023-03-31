b530381a45e4940e949a121eaac161ab2200401a
Ali <ninjawithcrown@gmail.ru>
2023-03-31 15:00:06 +0600

    start: portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*------------------------Search Drop-------------------------*/
+var searchButton = document.getElementById('search__icon');
+searchButton.addEventListener('click', function(event){
+  let elem = document.querySelector('.search');
+  elem.classList.toggle('_active');
+});
+/*-----------------------/Search Drop-------------------------*/
+
+console.log('Hello World!'); 
